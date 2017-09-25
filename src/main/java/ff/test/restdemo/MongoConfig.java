e4b5fb65cdf479d4fec0e3238092cb18b9f3f6e5
Florian Fusseder <f.fusseder@intence.de>
2017-09-25 17:53:43 +0200

    lines

--- src/main/java/ff/test/restdemo/MongoConfig.java
+++ src/main/java/ff/test/restdemo/MongoConfig.java
@@ -2,9 +2,13 @@ package ff.test.restdemo;
 
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableMongoRepositories
@@ -17,6 +21,10 @@ public class MongoConfig extends AbstractMongoConfiguration {
 
     @Override
     public Mongo mongo() throws Exception {
-        return new MongoClient();
+//        ServerAddress serverAddress = new ServerAddress("http://mongodb-restmongo.192.168.99.100.nip.io/", 27017);
+//        MongoCredential mongoCredential = MongoCredential.createMongoCRCredential("Florian", getDatabaseName(), "time2go".toCharArray());
+//        return new MongoClient(serverAddress, Arrays.asList(mongoCredential));
+        return new MongoClient("http://mongodb-restmongo.192.168.99.100.nip.io");
+
     }
 }
