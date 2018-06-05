1346cb6b95793fafe0d910da8eb72174432c87ea
FlorianFusseder <Florian@Florian-PC.localdomain>
2018-06-05 21:52:43 +0200

    changed customized request

--- src/main/java/ff/test/restdemo/Repository/CustomizedWrapperRepositoryImpl.java
+++ src/main/java/ff/test/restdemo/Repository/CustomizedWrapperRepositoryImpl.java
@@ -1,12 +1,15 @@
 package ff.test.restdemo.Repository;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import ff.test.restdemo.Pojo.Wrapper;
+import ff.test.restdemo.Pojo.Boy;
+import ff.test.restdemo.Pojo.Person;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+
+import java.util.List;
 
 public class CustomizedWrapperRepositoryImpl implements CustomizedWrapperRepository {
 
@@ -19,21 +22,13 @@ public class CustomizedWrapperRepositoryImpl implements CustomizedWrapperReposit
     }
 
     @Override
-    public Object getCompleteWrapperJackson() {
+    public List<Boy> getCompleteWrapperJackson() {
 
+        Boy boy = mongoTemplate.findAll(Boy.class).get(1);
+        String mongoId = boy.getMongoId();
+        Query id = Query.query(Criteria.where("bestFriend.$id").is(new ObjectId(mongoId)));
+        List<Boy> boys = mongoTemplate.find(id, Boy.class);
 
-        Wrapper object = mongoTemplate.findAll(Wrapper.class).get(0);
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
-
-        ObjectWriter objectWriter = objectMapper.writerFor(Wrapper.class);
-
-        try {
-            return objectWriter.writeValueAsString(object);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-
-        return "Fail";
+        return boys;
     }
 }
